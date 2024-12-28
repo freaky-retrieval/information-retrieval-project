4e09bf2f6699bb17abcac0acf4f6c6a688e35ac1
tcm03 <21125019@student.hcmus.edu.vn>
2024-12-28 08:30:53 +0000

    Pull

--- main.py
+++ main.py
@@ -5,14 +5,8 @@ from storages.milvus.milvus_db import MilvusDB
 import logging
 import os
 
-<<<<<<< HEAD
-MilvusDB.connect(
-    db_path = os.path.join(os.getcwd(), "infras/products.db")
-)
-=======
 
 MilvusDB.connect()
->>>>>>> develop-milvus
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
